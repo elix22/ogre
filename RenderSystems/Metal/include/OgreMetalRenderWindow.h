c00fddb306ba82f4a607e8055cfcca62bc410318
Pavel Rojtberg <rojtberg@gmail.com>
2020-03-22 17:59:24 +0100

    Metal: port to Ogre 1.12

--- RenderSystems/Metal/include/OgreMetalRenderWindow.h
+++ RenderSystems/Metal/include/OgreMetalRenderWindow.h
@@ -52,7 +52,7 @@ namespace Ogre
         id                  mResizeObserver;
 #endif
 
-        MetalRenderSystem   *mRenderSystem;
+        //MetalRenderSystem   *mRenderSystem;
 
         inline void checkLayerSizeChanges(void);
     public:
@@ -78,6 +78,8 @@ namespace Ogre
         virtual bool requiresTextureFlipping() const { return false; }
 
         virtual void getCustomAttribute( const String& name, void* pData );
+
+        PixelFormat suggestPixelFormat() const override { return mFormat; }
     };
 }
 
